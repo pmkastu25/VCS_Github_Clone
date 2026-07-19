1e15c186c28e044d6fbf220a81335e805931d58f
pmkastu25 <pmkastu25@gmail.com>
2026-07-19 19:16:48 +0530

    Done with PullRepo Logic

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,3 +1,33 @@
+import {promises as fs} from 'fs';
+import path from 'path';
+import {s3, s3Bucket} from '../config/aws-config.js';
+
 export async function pullRepo() {
-    console.log("Pull Command is called");
+    const repoPath = path.join(process.cwd(), '.mygit');
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        const data = await s3.listObjectsV2({ Bucket: s3Bucket, Prefix: 'commits/' }).promise();
+        const objects = data.Contents;
+
+        for(const object of objects) {
+            const key = object.Key;
+            const fileName = path.dirname(key).split('/').pop();
+
+            const commitsDir = path.join(commitsPath, fileName);
+
+            await fs.mkdir(commitsDir, { recursive: true });
+
+            const params = {
+                Bucket: s3Bucket,
+                Key: key
+            }
+
+            const fileContent = await s3.getObject(params).promise();
+            await fs.writeFile(path.join(repoPath, key), fileContent.Body);
+            console.log(`Pulled ${fileName} from S3 and saved to ${commitsDir}`);
+        }
+    } catch(err){
+        console.error('Error pulling repo:', err);
+    }
 }
