941f1d8654227d6afda8be8497dd7253dbb97ebc
pmkastu25 <pmkastu25@gmail.com>
2026-07-21 20:20:26 +0530

    Schema and Models defined for the database

---
+++ backend/models/repoModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+const RepositorySchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    content:[{
+        type: String
+    }],
+    visibility: {
+        type: Boolean
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true 
+    },
+    issues: [{
+        type: Schema.Types.ObjectId,
+        ref: "Issue",
+    }]
+})
+
+const Repository = mongoose.model("Repository", RepositorySchema);
+
+export default Repository;
---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    repositories: [{
+        default: [],
+        type: Schema.Types.ObjectId,
+        ref: "Repository"
+    }],
+    followedUsers: [{
+        default: [],
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    starRepos: [{
+        default: [],
+        type: Schema.Types.ObjectId,
+        ref: "Repository"
+    }],
+})
+
+const User = mongoose.model("Users", UserSchema);
+
+export default User;
